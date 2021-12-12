c9571c27e0f0028da5d72d683f9b63d561f833d5
Азат Сиразетдинов <azat222@gmail.com>
2021-12-12 16:25:22 +0500

    last_commit

--- main.py
+++ main.py
@@ -1,45 +1,63 @@
 import pygame as pg
+import pygame.freetype
 import sys
-
-WIDTH = 520
-HEIGHT = 360
-BLACK = (0, 0, 0)
+from random import uniform
+from settings import *
 
 
-class Ball():
-    def __init__(self, x, y, speed, x_, y_, surface, radius):
-        self.x = x
-        self.y = y
-        self.speed = speed
-        self.x_ = x_    # Направление движения
-        self.y_ = y_
-        self.s = surface
-        self.r = radius
+def buttons_handler(platformL, platformR):
+    pressed_buttons = pg.key.get_pressed()
+    if pressed_buttons[pg.K_ESCAPE]:
+        pg.quit()
+        sys.exit()
+    if pressed_buttons[pg.K_w]:
+        platformL = platformL.move(0, -move_value)
+    if pressed_buttons[pg.K_s]:
+        platformL = platformL.move(0, move_value)
+    if pressed_buttons[pg.K_UP]:
+        platformR = platformR.move(0, -move_value)
+    if pressed_buttons[pg.K_DOWN]:
+        platformR = platformR.move(0, move_value)
+    return platformL, platformR
 
-    def draw(self):
-        pg.draw.circle(self.s, (255, 255, 255), (self.x, self.y), self.r)
 
-    def move(self):
-        self.x += self.x_ * self.speed
-        self.y_ += self.y_ * self.speed
-        if self.x + self.r == WIDTH or self.x - self.r == 0:
-            self.x_ = -self.x_
+def draw_dots_line(x, y, delta, Surface):
+    for y_ in range(0, y, delta * 2):
+        pg.draw.line(Surface, WHITE, (x, y_), (x, y_ + delta), 3)
+
 
 def main():
     pg.init()
-    pg.display.set_mode((WIDTH, HEIGHT))
+    pg.display.set_mode((0, 0), pygame.FULLSCREEN)
     mainS = pg.display.get_surface()
-    FPS = 60
     clock = pg.time.Clock()
-    ball = Ball(WIDTH / 2, HEIGHT / 2, 4, 1, 1, mainS, 20)
+    xB, yB = HALF_WIDTH, HALF_HEIGHT
+    platformL = pygame.Rect(X_OFFSET, HALF_HEIGHT, 10, 50)
+    platformR = pygame.Rect(WIDTH - X_OFFSET, HALF_HEIGHT, 10, 50)
+    x_, y_ = 1, 1
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 pg.quit()
                 sys.exit()
+        platformL, platformR = buttons_handler(platformL, platformR)
         mainS.fill(BLACK)
-        ball.draw()
-        ball.move()
+        draw_dots_line(HALF_WIDTH, HEIGHT, 10, mainS)
+        circle = pg.draw.circle(mainS, WHITE, (xB, yB), 10)
+        if circle.top >= HEIGHT or circle.bottom <= 0:
+            y_ *= -1
+        if circle.right >= platformR.left and platformR.top <= circle.center[1] <= platformR.bottom:
+            x_ *= -1
+        elif circle.left <= platformL.right and platformL.top <= circle.center[1] <= platformL.bottom:
+            x_ *= -1
+        xB += x_ * speed
+        yB += y_ * speed
+        pg.draw.rect(mainS, WHITE, rect=platformL)
+        pg.draw.rect(mainS, WHITE, rect=platformR)
+        if circle.left < 0 or circle.right > WIDTH:
+            print('GAME OVER')
+            pg.quit()
+            sys.exit()
         pg.display.update()
         clock.tick(FPS)
 
---
+++ settings.py
@@ -0,0 +1,14 @@
+import pygame
+import pygame.freetype
+
+WIDTH, HEIGHT = 1280, 700
+speed = 4
+move_value = speed
+FPS = 60
+HALF_WIDTH = WIDTH / 2
+HALF_HEIGHT = HEIGHT / 2
+X_OFFSET = WIDTH * 0.01
+
+BLACK = pygame.Color('black')
+WHITE = pygame.Color('white')
+
